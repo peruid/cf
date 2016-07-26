462d7c2b64d54918bc6395e43553038c980256f9
Oleg Dambaev <o.dambaev@hostcomm.ru>
2016-07-26 14:44:54 +0300

    devopshost-1088: cosmetics and pslurp prepare: /var/tmp/stat_hostname.txt

--- hosting/devopshost-1088.sh
+++ hosting/devopshost-1088.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+HOST_SELF=$(hostname)
+STAT_FILE="/var/tmp/stat_{$HOST_SELF}.txt"
 cd /usr/sys/etc/farmusers
-for u in *;do WHEN=$(ls -l -D "%Y-%m-%d" $u/tariff | awk '{print $6}'); grep -q -E "_(((bitrix|wp|joom)[0-9]+)|(402|502|602))$" $u/tariff && ( printf "%s:DOGOVOR:" $u; cat $u/tariff; printf ":CREATE_DATE:%s:" $WHEN; cat $u/quota_disk; echo "");done
+for u in *;do WHEN=$(ls -l -D "%Y-%m-%d" $u/tariff | awk '{print $6}'); grep -q -E "_(((bitrix|wp|joom)[0-9]+)|(402|502|602))$" $u/tariff && ( printf "%s:DOGOVOR:" $u; cat $u/tariff; printf ":CREATE_DATE:%s:" $WHEN; cat $u/quota_disk; echo "");done > ${STAT_FILE}
