63ee450e6c2b04a6df1b75aae3e6626909e502cb
Oleg Dambaev <o.dambaev@hostcomm.ru>
2016-07-26 13:58:26 +0300

    devopshost-1088: add CREATE_DATE

--- hosting/devopshost-1088.sh
+++ hosting/devopshost-1088.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /usr/sys/etc/farmusers
-for u in *;do WHEN=$(ls -l -D "%Y-%m-%d" $u/tariff | awk '{print $6}'); grep -q -E "_(((bitrix|wp|joom)[0-9]+)|(402|502|602))$" $u/tariff && ( printf "%s:DOGOVOR:" $u; cat $u/tariff; printf ":%s:" $WHEN; cat $u/quota_disk; echo "");done
+for u in *;do WHEN=$(ls -l -D "%Y-%m-%d" $u/tariff | awk '{print $6}'); grep -q -E "_(((bitrix|wp|joom)[0-9]+)|(402|502|602))$" $u/tariff && ( printf "%s:DOGOVOR:" $u; cat $u/tariff; printf ":CREATE_DATE:%s:" $WHEN; cat $u/quota_disk; echo "");done
